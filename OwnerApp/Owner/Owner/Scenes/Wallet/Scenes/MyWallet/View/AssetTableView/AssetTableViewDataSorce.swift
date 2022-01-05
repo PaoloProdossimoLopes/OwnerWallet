67e030bdd9b51e1c409ba0ae52d608454dbd6d17
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-01-04 23:39:14 -0300

    add app icon in application

--- OwnerApp/Owner/Owner/Scenes/Wallet/Scenes/MyWallet/View/AssetTableView/AssetTableViewDataSorce.swift
+++ OwnerApp/Owner/Owner/Scenes/Wallet/Scenes/MyWallet/View/AssetTableView/AssetTableViewDataSorce.swift
@@ -25,6 +25,22 @@ final class AssetTableViewDataSorce: ATBVDSProtocol {
     init(assets: [AssetModel]) {
         self.assets = assets
     }
+    
+    //MARK: - Helpers
+    
+    private func createCell(tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
+        let defaultCell = UITableViewCell()
+        
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: AssetTableViewCell.registerName, for: indexPath
+        ) as? AssetTableViewCell else { return defaultCell }
+        
+        let assetModel = assets[indexPath.row]
+        cell.setAssetInformations(asset: assetModel)
+        cell.selectionStyle = .none
+        
+        return cell
+    }
 
 }
 
@@ -38,20 +54,6 @@ extension AssetTableViewDataSorce {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = createCell(tableView: tableView, indexPath: indexPath)
-        return cell
-    }
-    
-    private func createCell(tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
-        let defaultCell = UITableViewCell()
-        
-        guard let cell = tableView.dequeueReusableCell(
-            withIdentifier: AssetTableViewCell.registerName, for: indexPath
-        ) as? AssetTableViewCell else { return defaultCell }
-        
-        let assetModel = assets[indexPath.row]
-        cell.setAssetInformations(asset: assetModel)
-        cell.selectionStyle = .none
-        
         return cell
     }
     
